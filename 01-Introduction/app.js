1acbe72d4794156f51edc99bece6bbd646198c13
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-07 22:59:48 +0500

    Number and string types in Typescript

--- 01-Introduction/app.js
+++ 01-Introduction/app.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Creating variables with their types 
 let number1 = 10;
 let number2 = 20;
 let result = number1 + number2;
 console.log('Sum of ', number1, 'and', number2, '=', result);
+// Creating String variables in Typesscript 
+let userName = 'Muhammad Abdullah';
+let address = 'Dummy Addresss';
+console.log(userName);
 //# sourceMappingURL=app.js.map
--- 01-Introduction/app.ts
+++ 01-Introduction/app.ts
@@ -1,5 +1,12 @@
+// Creating number variables in Typescript
 let number1:number = 10
 let number2:number = 20
 let result:number = number1 + number2
-
 console.log('Sum of ', number1 , 'and', number2 , '=', result)
+
+
+// Creating String variables in Typesscript 
+let userName:string = 'Muhammad Abdullah'
+let address:string = 'Dummy Addresss'
+
+
