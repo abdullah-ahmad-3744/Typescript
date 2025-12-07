0e7d5948948d4b6750a092ef5d763e27dd77ff37
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-07 22:50:01 +0500

    Starting with the Typescript

---
+++ 01-Introduction/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let number1 = 10;
+let number2 = 20;
+let result = number1 + number2;
+console.log('Sum of ', number1, 'and', number2, '=', result);
+//# sourceMappingURL=app.js.map
