c37c395f008e9387dad9543613e78e7223328c62
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-08 17:23:02 +0500

    any data type in Typescript

--- 02-Type Inference/app.js
+++ 02-Type Inference/app.js
@@ -1,3 +1,16 @@
-let number = 10;
+let number1 = 10;
+//Type inference by Typescript Compiler
+let number2 = 20; // TS compiler will automatically assing the type number to it 
+// number2 = "Thirty" // It will give error becuase of the type number by type inference
+let name = "Ali";
+let userName = "Ahmad";
+// userName = true // again it will show error 
+// Any datatype in Typescript
+let balance;
+balance = 200;
+balance = "300";
+balance = true;
+balance = null;
+console.log(balance);
 export {};
 //# sourceMappingURL=app.js.map
--- 02-Type Inference/app.ts
+++ 02-Type Inference/app.ts
@@ -8,3 +8,13 @@ let number2=  20; // TS compiler will automatically assing the type number to it
 let name:string= "Ali"
 let userName = "Ahmad"
 // userName = true // again it will show error 
+
+
+// Any datatype in Typescript
+let address:any;
+let balance ;
+balance = 200
+balance = "300"
+balance = true
+balance = null
+console.log(balance)
