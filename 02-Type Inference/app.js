474f2b47474a27f3f138f73bc1613f2d1d0699e9
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-08 17:18:56 +0500

    Type inference in which Typescript compiler will automatically assign the type to the variables

---
+++ 02-Type Inference/app.js
@@ -0,0 +1,3 @@
+let number = 10;
+export {};
+//# sourceMappingURL=app.js.map
---
+++ 02-Type Inference/app.ts
@@ -0,0 +1,10 @@
+let number1:number = 10;
+
+
+//Type inference by Typescript Compiler
+let number2=  20; // TS compiler will automatically assing the type number to it 
+// number2 = "Thirty" // It will give error becuase of the type number by type inference
+
+let name:string= "Ali"
+let userName = "Ahmad"
+// userName = true // again it will show error 
