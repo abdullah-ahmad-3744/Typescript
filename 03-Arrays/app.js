1f1ab943c307d37abcd364ed754cca8d7328f1cf
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-09 15:07:29 +0500

    operations on mixed types array

--- 03-Arrays/app.js
+++ 03-Arrays/app.js
@@ -6,4 +6,9 @@ let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let array2 = [1, 2, 3, 4, 5, 6];
 // Automatically type will be assigned as string 
 let names = ['Ali', 'Ahmad', 'Hamza'];
+// Array containing mixed type values 
+let array3 = [1, 2, 3, 4, 'Ali', 'Hamza', true]; // here all types will be assigned automatically
+let array4 = [1, 2, 3, 4, 'Ali', 'Hamza', true];
+// Adding a vlaue other than above types will give error 
+array4.push(null); // shows error becuase null is not from the above types 
 //# sourceMappingURL=app.js.map
--- 03-Arrays/app.ts
+++ 03-Arrays/app.ts
@@ -8,3 +8,12 @@ let array2 = [1,2,3,4,5,6]
 
 // Automatically type will be assigned as string 
 let names= ['Ali', 'Ahmad', 'Hamza']
+
+
+// Array containing mixed type values 
+let array3 = [1,2,3,4, 'Ali', 'Hamza', true] // here all types will be assigned automatically
+let array4:(string | number | boolean)[] = [1,2,3,4, 'Ali', 'Hamza', true]
+
+
+// Adding a vlaue other than above types will give error 
+// array4.push(null)      // =====>  shows error becuase null is not from the above types 
