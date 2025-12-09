1c52eb53264ff72474b1ccb3aad46fe25ea33955
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-09 14:55:20 +0500

    Starting with the Arrays in Typescript

---
+++ 03-Arrays/app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Arrays in Typescript
+Object.defineProperty(exports, "__esModule", { value: true });
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//# sourceMappingURL=app.js.map
---
+++ 03-Arrays/app.ts
@@ -0,0 +1,3 @@
+// Arrays in Typescript
+
+let array:number[] = [1,2,3,4,5,6,7,8,9]
