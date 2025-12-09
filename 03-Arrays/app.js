512eda03e4245422928b84c5761f6fe78b1aec36
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-09 15:01:04 +0500

    Array syntax and thier types

--- 03-Arrays/app.js
+++ 03-Arrays/app.js
@@ -2,4 +2,8 @@
 // Arrays in Typescript
 Object.defineProperty(exports, "__esModule", { value: true });
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// This syntax is also valid and type number will be automatically assigned
+let array2 = [1, 2, 3, 4, 5, 6];
+// Automatically type will be assigned as string 
+let names = ['Ali', 'Ahmad', 'Hamza'];
 //# sourceMappingURL=app.js.map
--- 03-Arrays/app.ts
+++ 03-Arrays/app.ts
@@ -1,3 +1,10 @@
 // Arrays in Typescript
 
 let array:number[] = [1,2,3,4,5,6,7,8,9]
+
+
+// This syntax is also valid and type number will be automatically assigned
+let array2 = [1,2,3,4,5,6]
+
+// Automatically type will be assigned as string 
+let names= ['Ali', 'Ahmad', 'Hamza']
