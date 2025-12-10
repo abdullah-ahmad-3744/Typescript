40c8256406d08b4a480c52ac5ecdb5765da6fc6c
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-10 16:45:04 +0500

    Inline objects in Typescript

--- 04-Objects/objects.js
+++ 04-Objects/objects.js
@@ -1,9 +1,17 @@
 "use strict";
-// Objects in Typescript
 Object.defineProperty(exports, "__esModule", { value: true });
+// Objects in Typescript
+// Normal JS object
 const Objects = {
     name: "Muhammad Abdullah",
     age: 24,
     gender: "mail"
+};
+// Objects in Typescript
+// Simple way to create objects in Typescript
+const obj2 = {
+    name: "Abdullah Ahmad",
+    age: 25,
+    gender: "male"
 };
 //# sourceMappingURL=objects.js.map
--- 04-Objects/objects.ts
+++ 04-Objects/objects.ts
@@ -1,7 +1,16 @@
 // Objects in Typescript
-
+// Normal JS object
 const Objects = {
     name : "Muhammad Abdullah",
     age : 24,
     gender : "mail"
+}
+
+// Objects in Typescript
+
+// Inline objects in Typescript
+const obj2: {name:string, age:number, gender:string} = {
+    name : "Abdullah Ahmad",
+    age : 25,
+    gender : "male"
 }
