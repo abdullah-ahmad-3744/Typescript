92745a715d2ffe55f7fb95a31fa96d7cd2ee24b1
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-10 16:54:56 +0500

    Using Type Alice for creating the objects

--- 04-Objects/objects.js
+++ 04-Objects/objects.js
@@ -8,10 +8,31 @@ const Objects = {
     gender: "mail"
 };
 // Objects in Typescript
-// Simple way to create objects in Typescript
+// Inline objects in Typescript
 const obj2 = {
     name: "Abdullah Ahmad",
     age: 25,
+    gender: "male"
+};
+// Another way to create objetcs in Ts
+let obj3;
+obj3 = {
+    name: "Dummy User",
+    age: 30,
+    gender: "male"
+};
+// Now creating the Object from the Type alice "Customer"
+let customer1 = {
+    name: "Ali",
+    age: 23,
+    id: "fnoiuhj023j",
+    gender: 'male'
+};
+// Creating the Customer 2 object from the Alice type "Customer"
+let customer2 = {
+    name: "Furqan Riaz",
+    age: 22,
+    id: 'ihd9f2y3rhiuewd',
     gender: "male"
 };
 //# sourceMappingURL=objects.js.map
--- 04-Objects/objects.ts
+++ 04-Objects/objects.ts
@@ -14,3 +14,36 @@ const obj2: {name:string, age:number, gender:string} = {
     age : 25,
     gender : "male"
 }
+
+
+// Another way to create objetcs in Ts
+let obj3:{name:string; age:number;gender:string};
+obj3 = {
+    name : "Dummy User",
+    age : 30,
+    gender : "male"
+}
+
+
+//Using Type Alices for creating objects
+
+type customer = {
+    name:string,
+    age:number,
+    id:string,
+    gender:string
+}
+// Now creating the Object from the Type alice "Customer"
+let customer1:customer = {
+    name : "Ali",
+    age : 23,
+    id: "fnoiuhj023j",
+    gender : 'male'
+}
+// Creating the Customer 2 object from the Alice type "Customer"
+let customer2:customer = {
+    name : "Furqan Riaz",
+    age : 22,
+    id : 'ihd9f2y3rhiuewd',
+    gender:"male"
+}
