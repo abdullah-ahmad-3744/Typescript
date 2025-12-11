b816d74a3fab511bccb6aa19e1e863cc76f450b5
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-11 16:39:40 +0500

    Array of objects using Interface

--- 05-Array_of_Objects/Array_of_objects.js
+++ 05-Array_of_Objects/Array_of_objects.js
@@ -1,6 +1,7 @@
 "use strict";
 // Array of Objects
 Object.defineProperty(exports, "__esModule", { value: true });
+// In Javascript
 let array = [
     {
         name: "Abdullah Ahmad",
@@ -18,4 +19,29 @@ let array = [
         city: "Lahore"
     }
 ];
+// Array of objects 
+let users = [
+    {
+        name: "demo 1",
+        age: 31,
+        city: "Karachi"
+    },
+    {
+        name: "demo 2",
+        age: 25,
+        city: "Lahore"
+    }
+];
+let newUser1 = {
+    name: "Demo User",
+    age: 35,
+    address: "Demo Address",
+    isActive: true
+};
+let newUser2 = {
+    name: "Demo User 2",
+    age: 30,
+    address: "Demo Address 2",
+    isActive: false
+};
 //# sourceMappingURL=Array_of_objects.js.map
--- 05-Array_of_Objects/Array_of_objects.ts
+++ 05-Array_of_Objects/Array_of_objects.ts
@@ -18,3 +18,40 @@ let array = [
         city : "Lahore"
     }
 ]
+
+// Array of objects 
+let users: {name:string, age:number, city:string}[] =[
+    {
+        name : "demo 1",
+        age : 31,
+        city : "Karachi"
+    },
+    {
+        name : "demo 2",
+        age : 25,
+        city : "Lahore"
+    }
+]
+
+// Array of Objects using interfce
+interface userInterface {
+    name : string,
+    age : number,
+    address: string,
+    isActive : boolean
+}
+
+let newUser1 : userInterface = {
+    name : "Demo User",
+    age :35,
+    address : "Demo Address",
+    isActive : true
+}
+
+
+let newUser2 : userInterface = {
+    name : "Demo User 2",
+    age :30,
+    address : "Demo Address 2",
+    isActive : false
+}
