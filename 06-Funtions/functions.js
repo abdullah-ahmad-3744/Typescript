1568d18ffe6a1b863197de110f51ca9e1541cb4e
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-15 21:55:57 +0500

    Functions with default and Optional parameters

--- 06-Funtions/functions.js
+++ 06-Funtions/functions.js
@@ -12,4 +12,15 @@ function meet(msg, val) {
     console.log(msg, val);
 }
 meet("Abdullah Ahmad", 4);
+// Functions with default parameter
+function defaultParameter(msg = 'Hello') {
+    console.log(msg);
+}
+defaultParameter();
+// Functions with Optional parameter
+function oovertime(person) {
+    console.log(person || 'Ahmad khan');
+}
+oovertime("Ali");
+oovertime();
 //# sourceMappingURL=functions.js.map
--- 06-Funtions/functions.ts
+++ 06-Funtions/functions.ts
@@ -15,3 +15,19 @@ function meet(msg:string, val:number) {
     console.log(msg, val)
 }
 meet("Abdullah Ahmad", 4)
+
+
+// Functions with default parameter
+function defaultParameter (msg:string= 'Hello') {
+    console.log(msg)
+}
+defaultParameter()
+
+
+
+// Functions with Optional parameter
+function oovertime(person?:string) {
+    console.log(person || 'Ahmad khan')
+}
+oovertime("Ali") 
+oovertime() 
