0d203aa70c48b99161c5517cba11fb29db6581d5
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-15 21:50:50 +0500

    functions with arguments in Typescript

--- 06-Funtions/functions.js
+++ 06-Funtions/functions.js
@@ -1,4 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Functions in Typescript
+Object.defineProperty(exports, "__esModule", { value: true });
+// Function with one argument
+function greet(a) {
+    console.log(a);
+    return a + 5;
+}
+console.log(greet(10));
+// Function with two arguments
+function meet(msg, val) {
+    console.log(msg, val);
+}
+meet("Abdullah Ahmad", 4);
 //# sourceMappingURL=functions.js.map
--- 06-Funtions/functions.ts
+++ 06-Funtions/functions.ts
@@ -1,1 +1,17 @@
 // Functions in Typescript
+
+// Function with one argument
+function greet (a:number):number{
+    console.log(a)
+    return a+5
+}
+
+console.log(greet(10))
+
+
+
+// Function with two arguments
+function meet(msg:string, val:number) {
+    console.log(msg, val)
+}
+meet("Abdullah Ahmad", 4)
